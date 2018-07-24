a246fc8ffdfaaf3c7528fb3ed8327372ed30bfe8
saggiyogesh <saggiyogesh@gmail.com>
2018-07-24 17:08:25 +0530

    impl

---
+++ index.js
@@ -0,0 +1,68 @@
+import { AsyncStorage } from 'react-native';
+const autoLoadKeys = ['id_token', 'access_token', 'me'];
+
+const _store = {};
+
+(async function() {
+  try {
+    for (const key of autoLoadKeys) {
+      _store[key] = await AsyncStorage.getItem(key);
+    }
+  } catch (err) {
+    console.log('RN Strorage, err: ', err);
+  }
+})();
+
+function save(key, value) {
+  _store[key] = value;
+  AsyncStorage.setItem(key, value).catch(e => console.log('RNStrorage,  err save', e));
+}
+
+function load(key) {
+  return _store[key];
+}
+
+function remove(key) {
+  delete _store[key];
+  AsyncStorage.removeItem(key).catch(e => console.log('RNStrorage,  err remove', e));
+}
+
+function loadAll() {
+  return _store;
+}
+
+export default { save, load, loadAll, remove };
+import { AsyncStorage } from 'react-native';
+const autoLoadKeys = ['id_token', 'access_token', 'me'];
+
+const _store = {};
+
+(async function() {
+  try {
+    for (const key of autoLoadKeys) {
+      _store[key] = await AsyncStorage.getItem(key);
+    }
+  } catch (err) {
+    console.log('RN Strorage, err: ', err);
+  }
+})();
+
+function save(key, value) {
+  _store[key] = value;
+  AsyncStorage.setItem(key, value).catch(e => console.log('RNStrorage,  err save', e));
+}
+
+function load(key) {
+  return _store[key];
+}
+
+function remove(key) {
+  delete _store[key];
+  AsyncStorage.removeItem(key).catch(e => console.log('RNStrorage,  err remove', e));
+}
+
+function loadAll() {
+  return _store;
+}
+
+export default { save, load, loadAll, remove };
