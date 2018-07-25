739e42f40346043b9103598f4804499deac3cc61
Yogesh Agrawal <saggiyogesh@gmail.com>
2018-07-25 11:01:45 +0530

    init method

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-import { AsyncStorage } from "react-native";
-const autoLoadKeys = ["id_token", "access_token", "me"];
+import { AsyncStorage } from 'react-native';
+const autoLoadKeys = ['id_token', 'access_token', 'me'];
 
 const _store = {};
 
-(async function() {
+async function init() {
   try {
     for (const key of autoLoadKeys) {
       _store[key] = await AsyncStorage.getItem(key);
     }
   } catch (err) {
-    console.log("RN Strorage, err: ", err);
+    console.log('RN Strorage, err: ', err);
   }
-})();
+}
 
 function save(key, value) {
   _store[key] = value;
-  AsyncStorage.setItem(key, value).catch(e =>
-    console.log("RNStrorage,  err save", e)
-  );
+  AsyncStorage.setItem(key, value).catch(e => console.log('RNStrorage,  err save', e));
 }
 
 function load(key) {
@@ -26,13 +24,11 @@ function load(key) {
 
 function remove(key) {
   delete _store[key];
-  AsyncStorage.removeItem(key).catch(e =>
-    console.log("RNStrorage,  err remove", e)
-  );
+  AsyncStorage.removeItem(key).catch(e => console.log('RNStrorage,  err remove', e));
 }
 
 function loadAll() {
   return _store;
 }
 
-export default { save, load, loadAll, remove };
+export default { save, load, loadAll, remove, init };
